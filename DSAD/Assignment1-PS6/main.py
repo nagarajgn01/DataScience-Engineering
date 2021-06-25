ab8667dad48a8403ec5a855ee1585bef21cc660f
Nagaraj Nayak <nagarajgn01@live.com>
2021-06-25 21:52:18 +0530

    Optimized code

--- DSAD/Assignment1-PS6/main.py
+++ DSAD/Assignment1-PS6/main.py
@@ -26,79 +26,74 @@ class bookNode:
             self.avCntr = availCount
     
     def _chkInChkOut(self,bkID,inOut):
-        self.updateData(bkID,inOut,self)        
-        
-    def updateData(self,bkID,inOut,root):
-        if root:
-            if root.bookID==bkID:
+        if self.bookID:
+            if self.bookID==bkID:
                 if inOut=='checkOut':
-                    root.avCntr-=1
-                    root.chkOutCntr+=1
+                    self.avCntr-=1
+                    self.chkOutCntr+=1
                 if inOut=='checkIn':
-                    root.avCntr+=1
-                return                
-            self.updateData(bkID,inOut,root.left)            
-            self.updateData(bkID,inOut,root.right)
-        return
-    
-      
-    def getAllData(self,root):
+                    self.avCntr+=1
+            elif bkID<self.bookID:
+                if self.left is not None:
+                    self.left._chkInChkOut(bkID,inOut)
+            else:
+                if self.right is not None:
+                    self.right._chkInChkOut(bkID,inOut)    
+
+    def getBookList(self,bkNode):
         res = []
-        if root:
-            res.append((root.bookID,root.chkOutCntr,root.avCntr))
-            if root.left:
-                res = res+self.getAllData(root.left)
-            if root.right:
-                res = res+self.getAllData(root.right)
-        else:
-            return
+        if bkNode:
+            res = self.getBookList(bkNode.left)
+            res.append((bkNode.bookID,bkNode.chkOutCntr,bkNode.avCntr))            
+            res = res + self.getBookList(bkNode.right)
         return res
-            
-    def _getTopBooks(self):        
-        lt = self.getAllData(self)
-        lt.sort(key=lambda x:x[1],reverse=True)
+
+    def _getTopBooks(self,bkNode):
+        res = self.getBookList(bkNode)
+        res.sort(key=lambda x:x[1],reverse=True)
         file = open(dir+'\outputPS6.txt','a')
         string = ''
-        for i,x in enumerate(lt[:3]):
+        for i,x in enumerate(res[:3]):
             string=string+'Top Books '+str(i+1)+': '+str(x[0])+', '+str(x[1])+'\n'  
         string = string +'\n'
-        print(string)
-        file.write(string)
-        file.close()
-    
-    def printBooks(self):
-        lt = self.getAllData(self)
-        file = open(dir+'\outputPS6.txt','a')
-        string = 'There are a total of '+str(len(lt))+' book titles in the library.\n'
-        for i,x in enumerate(sorted(lt)):
-            string = string + str(x[0])+', '+str(x[2])+'\n'
-        string = string +'\n'
-        print(string)
         file.write(string)
         file.close()
         
-    def _findBook(self, bkID):
-        string = self.searchInventory(bkID,self)
+    def searchInventory(self,eNode, bkID):
+        string=''
+        if eNode.bookID:
+            if eNode.bookID==bkID:
+                if eNode.avCntr>0:
+                    string = 'Book id '+str(bkID)+' is available for checkout\n\n'
+                elif eNode.avCntr==0:
+                    string = 'All copies of book id '+str(bkID)+' have been checked out\n\n'
+                return string
+            elif bkID<eNode.bookID:  
+                if eNode.left is not None:
+                    string = string + self.searchInventory(eNode.left,bkID) 
+            else:           
+                if eNode.right is not None: 
+                    string = string + self.searchInventory(eNode.right,bkID)
+        return string   
+
+    def _findBook(self, eNode, bkID):
+        string = self.searchInventory(eNode, bkID)
         if string=='':
             string = 'Book id '+str(bkID)+' does not exist.\n\n'
-        print(string)
         file = open(dir+'\outputPS6.txt','a')
         file.write(string)
-        file.close()            
-        
-        
-    def searchInventory(self, bkID, root):
-        string=''
-        if root:
-            if root.bookID==bkID:
-                if root.avCntr>0:
-                    string = 'Book id '+str(bkID)+' is available for checkout\n\n'
-                elif root.avCntr==0:
-                    string = 'All copies of book id '+str(bkID)+' have been checked out\n\n'
-                return string               
-            string = string + self.searchInventory(bkID,root.left)            
-            string = string + self.searchInventory(bkID,root.right)
-        return string
+        file.close()     
+
+    def printBooks(self,bkNode):
+        lt = self.getBookList(bkNode)
+        file = open(dir+'\outputPS6.txt','a')
+        string = 'There are a total of '+str(len(lt))+' book titles in the library.\n'
+        for i,x in enumerate(lt):
+            string = string + str(x[0])+', '+str(x[2])+'\n'
+        string = string +'\n'
+        file.write(string)
+        file.close()       
+
         
 if __name__=='__main__':
     obj = bookNode()
@@ -110,11 +105,11 @@ if __name__=='__main__':
         for row in csv_reader:
             obj._readBookList(int(row[0].strip()),int(row[1].strip()))
             
-    with open(dir+'\promptsPS6.txt', 'r') as read_obj:
+    with open(dir+'\promptsPS6_test.txt', 'r') as read_obj:
         csv_reader = reader(read_obj)
         for row in csv_reader:
-            #print(row)
             if len(row)>0:
+                #print(row)
                 if ':' in row[0]:
                     if 'check' in row[0]:
                         inOut,bkID  = row[0].split(':')
@@ -122,23 +117,10 @@ if __name__=='__main__':
                         continue
                     if 'find' in row[0]:
                         inOut,bkID  = row[0].split(':')
-                        obj._findBook(int(bkID.strip()))
+                        obj._findBook(obj,int(bkID.strip()))
                 if 'ListTopBooks' in row[0]:
-                    obj._getTopBooks()
+                    obj._getTopBooks(obj)
                     continue
                 if 'printInventory' in row[0]:
-                    obj.printBooks()
+                    obj.printBooks(obj)
                     continue
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
